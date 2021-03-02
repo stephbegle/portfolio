3e25708f5625a2d6fa55830c1dc07f0e6401dd72
stephbegle <stephbegle@gmail.com>
2021-03-01 21:40:04 -0800

    added a list of commented out requirements i would like to have in my portfolio to the gitignore file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <>
+
     <h2>Stephanie Begle</h2>
+    <div>
+      <p>About me section</p>
+    </div>
+
+
+    </>
   );
 }
 

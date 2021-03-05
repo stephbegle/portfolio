8688a6248550672a363b346356ad2bb9ab9e8e6d
stephbegle <stephbegle@gmail.com>
2021-03-04 18:21:24 -0800

    about me page layout in

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
+import author from "../DSC_0542.jpg";
 
 const AboutMe = () => {
     return (
         <>
-            <div className="me-wraper">
-                <div className="about-me">
-                    <h1>About Me</h1>
-                    <div className="me-info">
-                        I graduated from the University of California San Diego with a degree in Political Science - Data Analytics. My initial path was in the social science direction, as I am passionate about state and social affairs blah blah blah. Here is some more sentences I will later insert about me and my background in a fun way maybe perhaps I guess.
-                        Maybe try to keep in shorter and sweeter.
-                    </div>
-
-                </div>
+        <div className="container-about py-5">
+        <h1 className="header-about">About Me</h1>
+            <div className="row">
                 
+                <div className="col-lg-6 col-xm-12" /* creates a column space on the first half of the page from left to right (so the left side here) */ >
+                    <p className="ml-1">Here is some information about myself! There will be more content written in here when I get a chance, so this is just a wordy placeholder useful for envisioning what the final product may look closely to. I want to make this long enough so I can see how the paragraph wraps around the page with this column row class coming from Bootstap. And I think that's enough writing there.</p>
+                </div>
+                <div className="col-lg-6 col-xm-12" /* the right side here */ >
+                    <div className="img-wrap mb-5 mt-2" /* mb margin bottom mt top (bootstrap feature) */ >
+                        <img className="profile-img" src={author} alt="author"/>
+                    </div>
+                </div>
+
             </div>
+            
+        </div>
         </>
     )
 }

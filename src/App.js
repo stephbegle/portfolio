cad1b23eb34f7553e0bfb0ec1fca5972119f0297
stephbegle <stephbegle@gmail.com>
2021-03-27 20:02:01 -0700

    saving some services icons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Services from "./components/Services";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     <Header />
     <AboutMe />
     <Experience />
+    <Services />
     <Projects />
     <Contact />
     <Footer />
---
+++ src/components/Services.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { FaGithub,
+    FaNodeJs,
+    FaNode
+} from "react-icons/fa";
+import { SiJavascript,
+    SiRuby,
+    SiPython,
+    SiReact,
+    SiRails, 
+    SiPostgresql,
+    SiAdobelightroomcc,
+    SiAdobephotoshop
+} from "react-icons/si"
+import { AiOutlineConsoleSql,
+    AiFillGithub 
+} from "react-icons/ai";
+import { BiGitBranch } from "react-icons/bi";
+
+const Services = () => {
+    return (
+
+        <div className="project-wrapper">
+            <div className="container">
+                <h1 className="text-center py-5">Services</h1>
+            </div>
+            <div className="services-icons">
+            <SiJavascript 
+            className="icon" size={60} />
+            <SiRuby 
+            className="icon" size={60} />
+            <FaNode 
+            className="icon" size={90} />
+            <SiPython 
+            className="icon" size={60} />
+            <br />
+            <SiReact 
+            className="icon" size={60} />
+            <SiRails
+            className="icon" size={90} />
+            <SiPostgresql
+            className="icon" size={60} />
+            <AiOutlineConsoleSql 
+            className="icon" size={70} />
+            <br />
+            <BiGitBranch 
+            className="icon" size={60} />
+            <AiFillGithub 
+            className="icon" size={60} />
+            <SiAdobelightroomcc 
+            className="icon" size={55} />
+            <SiAdobephotoshop 
+            className="icon" size={55} />
+            </div>
+        </div>
+    
+    )
+}
+
+export default Services

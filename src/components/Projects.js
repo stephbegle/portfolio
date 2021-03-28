4ceb29073153daf388b0ca595b8c7eafdeb0a6f1
stephbegle <stephbegle@gmail.com>
2021-03-27 20:18:00 -0700

    saving project changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,58 @@
-import React from 'react'
-import cardcss from "../project-imgs/cardcss.png"
-import holiday from "../project-imgs/holiday.png"
-import theme from "../project-imgs/theme.png"
+import React from 'react';
+import cardcss from "../project-imgs/cardcss.png";
+import holiday from "../project-imgs/holiday.png";
+import theme from "../project-imgs/theme.png";
+import Clogs from "../project-imgs/clogs-game.png";
+import TicTacToe from "../project-imgs/tictac.png";
 import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
+import { FaBlackberry } from 'react-icons/fa';
 
 const Projects = () => {
-    // cardcss
-    const openPopupboxCardcss = () => {
+    // Clogs
+    const openPopupboxClogs = () => {
         const content = (
-          <>
-        <img src={cardcss} alt="Cardcss project" />
-        <p>This is a description of the project.</p>
-        <b>Github:</b><a className="hyper-link" onClick={() => window.open("https://github.com/stephbegle/game-projects")}>https://github.com/stephbegle/game-projects</a>
+        <>
+        <img className="projects-image-popupbox" src={Clogs} alt="Clogs Game project" />
+        <p className="para-desc">This is a description of the project.</p>
+        <b className="link-name">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/stephbegle/game-projects")}>https://github.com/stephbegle/game-projects</a>
         </>  
         )
         
         PopupboxManager.open({content})
     }
 
-    const popupboxConfigCardcss = {
+    const popupboxConfigClogs = {
+        titleBar:{
+            enable: true,
+            text: "Clogs Game Project",
+        },
+        fadeIn: true,
+        fadeInSpeed: 500,
+        fadeOutSpeed: 200
+    }
 
+    // TicTacToe
+    const openPopupboxTicTacToe = () => {
+        const content = (
+        <>
+        <img className="projects-image-popupbox" src={TicTacToe} alt="Tic Tac Toe Game project" />
+        <p className="para-desc">This is a description of the project.</p>
+        <b className="link-name">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/stephbegle/game-projects")}>https://github.com/stephbegle/game-projects</a>
+        </>  
+        )
+        
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigTicTacToe = {
+        titleBar:{
+            enable: true,
+            text: "Tic Tac Toe Game Project",
+        },
+        fadeIn: true,
+        fadeInSpeed: 500,
+        fadeOutSpeed: 200
     }
 
     return (
@@ -28,13 +61,13 @@ const Projects = () => {
                 <h1 className="text-center py-5">Projects</h1>
                         {/*                          */}
                 <div className="image-box-wrapper row justify-content-center">
-                    <div className="project-image-box" onClick={openPopupboxCardcss}>
-                        <img className="project-image" src={cardcss} alt="cardcss example" />
+                    <div className="project-image-box" onClick={openPopupboxClogs}>
+                        <img className="project-image" src={Clogs} alt="Clogs Game example" />
                         <div className="overflow"></div>
                     </div>
                         {/*                          */}
-                    <div className="project-image-box">
-                        <img className="project-image" src={holiday} alt="holiday example" />
+                    <div className="project-image-box" onClick={openPopupboxTicTacToe}>
+                        <img className="project-image" src={TicTacToe} alt="Tic Tac Toe example" />
                         <div className="overflow"></div>
                     </div>
                         {/*                          */}
@@ -44,7 +77,7 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-            <PopupboxContainer {...popupboxConfigCardcss}/>
+            <PopupboxContainer {...popupboxConfigClogs}/>
         </div>
     )
 }

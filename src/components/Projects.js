40a50ed3e543360ff1435843ade1f8401aa683b4
stephbegle <stephbegle@gmail.com>
2021-04-03 21:06:10 -0700

    added piglatin proj

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import cardcss from "../project-imgs/cardcss.png";
-import holiday from "../project-imgs/holiday.png";
-import theme from "../project-imgs/theme.png";
-import Clogs from "../project-imgs/clogs-game.png";
-import TicTacToe from "../project-imgs/tictac.png";
-import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import React from "react";
+import clogs from "../project-imgs/clogs-game.png";
+import tictactoe from "../project-imgs/tictac.png";
+import piglatin from "../project-imgs/pig-latin.png"
 import "react-popupbox/dist/react-popupbox.css";
-import { FaBlackberry } from 'react-icons/fa';
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 
 const Projects = () => {
     // Clogs
     const openPopupboxClogs = () => {
         const content = (
         <>
-        <img className="projects-image-popupbox" src={Clogs} alt="Clogs Game project" />
+        <img className="projects-image-popupbox" src={clogs} alt="Clogs Game project" />
         <p className="para-desc">This is a description of the project.</p>
         <b className="link-name">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/stephbegle/game-projects")}>https://github.com/stephbegle/game-projects</a>
         </>  
@@ -36,7 +33,7 @@ const Projects = () => {
     const openPopupboxTicTacToe = () => {
         const content = (
         <>
-        <img className="projects-image-popupbox" src={TicTacToe} alt="Tic Tac Toe Game project" />
+        <img className="projects-image-popupbox" src={tictactoe} alt="Tic Tac Toe game project" />
         <p className="para-desc">This is a description of the project.</p>
         <b className="link-name">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/stephbegle/game-projects")}>https://github.com/stephbegle/game-projects</a>
         </>  
@@ -45,10 +42,33 @@ const Projects = () => {
         PopupboxManager.open({content})
     }
 
+    
     const popupboxConfigTicTacToe = {
         titleBar:{
             enable: true,
             text: "Tic Tac Toe Game Project",
+        },
+        fadeIn: true,
+        fadeInSpeed: 500,
+        fadeOutSpeed: 200
+    }
+    
+    const openPopupboxPig = () => {
+        const content = (
+        <>
+        <img className="projects-image-popupbox" src={piglatin} alt="Pig Latin translator project" />
+        <p className="para-desc">This is a simple pig latin translator with some fun styling.</p>
+        <b className="link-name">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/stephbegle/game-projects")}>https://github.com/stephbegle/game-projects</a>
+        </>  
+        )
+        
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigPig = {
+        titleBar:{
+            enable: true,
+            text: "Pig Latin Translator Project",
         },
         fadeIn: true,
         fadeInSpeed: 500,
@@ -61,18 +81,18 @@ const Projects = () => {
                 <h1 className="text-center py-5"><span style={{color:"var(--primary-pearl-white)"}}>Projects</span></h1>
                         {/*                          */}
                 <div className="image-box-wrapper row justify-content-center">
-                    <div className="project-image-box" onClick={openPopupboxClogs}>
-                        <img className="project-image" src={Clogs} alt="Clogs Game example" />
+                    <div className="project-image-box" onClick={ openPopupboxClogs }>
+                        <img className="project-image" src={clogs} alt="Clogs Text Game" />
                         <div className="overflow"></div>
                     </div>
                         {/*                          */}
-                    <div className="project-image-box" onClick={openPopupboxTicTacToe}>
-                        <img className="project-image" src={TicTacToe} alt="Tic Tac Toe example" />
+                    <div className="project-image-box" onClick={ openPopupboxTicTacToe }>
+                        <img className="project-image" src={tictactoe} alt="Tic Tac Toe Game" />
                         <div className="overflow"></div>
                     </div>
                         {/*                          */}
-                    <div className="project-image-box">
-                        <img className="project-image" src={theme} alt="theme example" />
+                    <div className="project-image-box" onClick={ openPopupboxPig }>
+                        <img className="project-image" src={piglatin} alt="Pig Latin Translator" />
                         <div className="overflow"></div>
                     </div>
                 </div>

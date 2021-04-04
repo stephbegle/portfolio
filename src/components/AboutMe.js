ecd5a47aef277feeda870230f198cddd62bd2529
stephbegle <stephbegle@gmail.com>
2021-04-03 20:44:03 -0700

    adjusted profile image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
                     <p>I'm a <span style={{color:"aqua"}}>software developer</span> based in San Diego, CA who has grown to love building things for the internet. I began my educational journey with the study of Political Science with a focus in Data Analytics at the <span style={{color:"aqua"}}>University of California San Diego</span> and quickly realized the grander curiosity I had for the analytics side of the field, which later prompted my transition into web development!</p>
                     <p>I've been working on building cool web apps using the <span style={{color:"aqua"}}>JavaScript</span> and <span style={{color:"aqua"}}>Ruby</span> languages, and navigating through <span style={{color:"aqua"}}>React</span> and <span style={{color:"aqua"}}>Rails</span> for launching them.</p>
                 </div>
-                <div className="col-lg-6 col-xm-12" /* the right side here */ >
+                <div className="entire-img col-lg-6 col-xm-12" /* the right side here */ >
                     <div className="img-wrap mb-5 mt-2" /* mb margin bottom mt top (bootstrap feature) */ >
                         <img className="profile-img" src={author} alt="author"/>
                     </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,10 +11,9 @@ const Experience = () => {
                     <div className="marker"></div>
                     <div className="timeline-content">
                         <h5>Learn Academy Bootcamp</h5>
-                        <h6>
+                        <h6 className="dates-right">
                             <span style={{color:"var(--primary-aqua)"}}>Feb 2021 - April 2021</span>
-                        </h6>
-                        <h7>San Diego, Ca</h7>
+                        <br />La Jolla, Ca</h6>
                         <ul className="bullets">
                             <li className="exp-content-pad">Devoted 480+ hours to building full stack web aulplications using React and Ruby on Rails with JavaScript and Ruby languages.</li><li>Created functional web applications using API with the Rails framework.</li>Developed best practices for version control of full stack applications utilizing pair and mob programming practices with peers. <li>Improved applications using debugging techniques and test driven development with Jest and Rspec.</li>
                         </ul>
@@ -25,10 +24,9 @@ const Experience = () => {
                     <div className="marker"></div>
                     <div className="timeline-content">
                         <h5 className="year-pos-right">Sales Specialist - Apple, Inc.</h5>
-                        <h6 className="dates">
+                        <h6 className="dates-left">
                             <span style={{color:"var(--primary-aqua)"}}>Oct 2019 - March 2020</span>
-                        </h6>
-                        <h7 className="city-name">La Jolla, Ca</h7>
+                        <br/> Ja Jolla, Ca</h6>
                         <ul className="bullets">
                             <li className="exp-content-pad">Researched product functionalities to aid customer understanding of them with helpful analogies.</li><li>Created functional web applications using API with the Rails framework.</li>Promoted Apple services that would be helpful for user experiences and increased insurance enrollments (AppleCare+) by about 25%.  <li>Helped customers understand phone service activation requirements and steps.</li>
                         </ul>
@@ -39,10 +37,9 @@ const Experience = () => {
                     <div className="marker"></div>
                     <div className="timeline-content">
                     <h5 className="year-pos-left">Chief of Marketing - UCSD Student Org.</h5>
-                    <h6>
+                    <h6 className="dates-right">
                         <span style={{color:"var(--primary-aqua)"}}>Sept 2020 - June 2020</span>
-                    </h6>
-                    <h7>La Jolla, Ca</h7>
+                    <br />La Jolla, Ca</h6>
                         <ul className="bullets">
                             <li className="exp-content-pad">Managed social media accounts and collaborated with UC departments on community development projects. </li><li>Designed flyers and logos for on-campus events.</li>
                         </ul>
@@ -53,10 +50,9 @@ const Experience = () => {
                     <div className="marker"></div>
                     <div className="timeline-content">
                     <h5 className="year-pos-right">Office Manager - DFO Global Commerce</h5>
-                    <h6 className="dates">
+                    <h6 className="dates-left">
                         <span style={{color:"var(--primary-aqua)"}}>June 2018 - Sept 2018</span>
-                    </h6>
-                    <h7>San Diego, Ca</h7>
+                    <br />La Jolla, Ca</h6>
                         <ul className="bullets">
                             <li className="exp-content-pad">Organized expense reports along with setting up 50+ company credit cards with major credit card companies on a monthly basis.</li><li>Managed inventory and purchases of supplies for daily office operations.</li>
                         </ul>
